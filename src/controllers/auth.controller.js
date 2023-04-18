c27e37f85e1c6d061ef74b44d008d57956215ff8
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-04-18 16:49:28 +0700

    create: forgot and reset password (authentication)

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,4 +1,6 @@
 const userModel = require("../models/users.model")
+const profileModel = require("../models/profile.model")
+const forgotRequestModel = require("../models/forgotRequest.model")
 const errorHandler = require("../helpers/errorHandler.helper")
 const jwt = require("jsonwebtoken")
 const {APP_SECRET} = process.env
@@ -28,7 +30,7 @@ exports.login = async (request, response) => {
 
 exports.register = async (request, response) => {
     try {
-        const {password, confirmPassword} = request.body
+        const {fullName, password, confirmPassword} = request.body
         if(password !== confirmPassword) {
             throw Error("password_unmatch")
         }
@@ -37,7 +39,12 @@ exports.register = async (request, response) => {
             ...request.body,
             password: hash
         }
-        const user = await userModel.insert(data)
+        const user = await userModel.insert(data) //insert ke table users
+        const profileData = {
+            fullName,
+            userId: user.id
+        }
+        await profileModel.insert(profileData)  //insert ke tabel profile
         const token = jwt.sign({id: user.id}, APP_SECRET)
         return response.json({
             success: true,
@@ -45,6 +52,56 @@ exports.register = async (request, response) => {
             results: {token}
         })
     } catch (error){
+        return errorHandler(response, error)
+    }
+}
+exports.forgotPassword = async (request, response) => {
+    try {
+        const {email} = request.body
+        const user = await userModel.findOneByEmail(email)
+        if(!user){
+            throw Error("no_user")
+        }
+        const randomNumber = Math.random()
+        const rounded = Math.round(randomNumber * 100000)
+        const padded = String(rounded).padEnd(6, "0")
+        
+        const forgot = await forgotRequestModel.insert({
+            email: user.email,
+            code: padded
+        })
+        if(!forgot){
+            throw Error("forgot_failed")
+        }
+        return response.json({
+            success: true,
+            message: "Request reset password success"
+        })
+    } catch(error) {
+        return errorHandler(response, error)
+    }
+}
+
+exports.resetPassword = async (request, response) => {
+    try{
+        const {code, email, password} = request.body
+        const find = await forgotRequestModel.findOneByCodeAndEmail(code, email)
+        if(!find){
+            throw Error("no_forget_requested")
+        }
+        const selectedUser = await userModel.findOneByEmail(email)
+        const data = {
+            password: await argon.hash(password)
+        }
+        const user = await userModel.update(selectedUser.id, data)
+        if(!user){
+            throw Error("no_forget_requested")
+        }
+        return response.json({
+            success: true,
+            message: "Reset password success"
+        })
+    }catch(error){
         return errorHandler(response, error)
     }
 }
--- src/routers/auth.router.js
+++ src/routers/auth.router.js
@@ -6,5 +6,7 @@ const authController = require("../controllers/auth.controller")
 
 authRouter.post("/login", validate("authLogin"), authController.login)
 authRouter.post("/register", authController.register)
+authRouter.post("/forgot-password", authController.forgotPassword)
+authRouter.post("/reset-password", validate("resetPassword"), authController.resetPassword)
 
 module.exports = authRouter
