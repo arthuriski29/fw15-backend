6851a4ddcca19dbf811e2328f059b08a61fdfa5c
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-05-01 00:44:16 +0700

    fix: fileRemover (update), try&catch

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -1,28 +1,49 @@
+const errorHandler = require("../helpers/errorHandler.helper")
 const fileRemover = require("../helpers/fileRemover.helper")
 const profileModel = require("../models/profile.model")
 
 exports.updateProfile = async(req, res) => {
-    const {id} = req.user
-    const user = await profileModel.findOneByUserId(id)
-    console.log(user)
-    const data = {
-        ...req.body
-    }
-    if(req.file){
-        if(user.picture){
-            console.log(user.picture)
-            fileRemover({filename: user.picture})
+    try {
+        const {id} = req.user
+        const user = await profileModel.findOneByUserId(id)
+        console.log(user)
+        const data = {
+            ...req.body
         }
-        data.picture = req.file.filename
-    }
-    const profile = await profileModel.updateByUserId(id, data)
-    if(!profile){
-        throw Error("update_profile_failed")
-    }
-    return res.json({
-        success: true,
-        message: "Profile updated",
-        results: profile
+        if(req.file){
+            if(user.picture){
+                console.log(user.picture)
+                fileRemover({filename: user.picture})
+            }
+            data.picture = req.file.filename
+        }
+        const profile = await profileModel.updateByUserId(id, data)
+        if(!profile){
+            throw Error("update_profile_failed")
+        }
+        return res.json({
+            success: true,
+            message: "Profile updated",
+            results: profile
       
-    })
+        })
+    } catch (error) {
+        return errorHandler(res, error)
+    }
+}
+exports.getProfile = async (req, res) => {
+    try {
+        const {id} = req.user
+        const profile = await profileModel.findOneByUserId(id)
+        if(!profile){
+            throw Error("profile_not_found")
+        }
+        return res.json({
+            success: true,
+            message: "Profile",
+            results: profile
+        })
+    } catch (error) {
+        return errorHandler(res, error)
+    }
 }
