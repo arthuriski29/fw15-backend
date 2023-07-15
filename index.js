43236285a098b8c96ca153e4b2fb54f03301e146
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-07-15 19:07:59 +0700

    add: cors & multer

--- index.js
+++ index.js
@@ -3,9 +3,29 @@ require("dotenv").config({
 })
 
 const express = require("express")
+const cors = require("cors")
 
 const app = express()
 app.use(express.urlencoded({extended: false}))
+
+// app.use(cors({
+//     origin: "http://localhost:5173",
+//     optionsSuccessStatus: 200
+// }))
+
+var whitelist = ["http://localhost:5173", "http://127.0.0.1:5173"]
+var corsOptions = {
+    origin: function (origin, callback) {
+        if ((origin === undefined ) || (whitelist.indexOf(origin) !== -1)) {
+            callback(null, true)
+        } else {
+            callback(new Error("Not allowed by CORS"))
+        }
+    }
+}
+app.use(cors(corsOptions))
+
+app.use("/uploads", express.static("uploads"))
 
 app.use("/", require("./src/routers/index"))
 
