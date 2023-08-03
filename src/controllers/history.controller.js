df0f4f315fe913855dceea8a9bbbc88d2372c1b7
arthuriski29 <78630365+arthuriski29@users.noreply.github.com>
2023-08-04 00:45:26 +0700

    fix: error for no history data

--- src/controllers/history.controller.js
+++ src/controllers/history.controller.js
@@ -3,6 +3,12 @@ const rsvModel = require("../models/reservations.model")
 exports.getAllHistory = async (req, res)=> {
     const {id: userId} = req.user
     const histories = await rsvModel.findAllByUserId(userId, req.query)
+    if(!histories){
+        return res.json({
+            success: false,
+            message: "This user have not book any transaction yet"
+        })
+    }
     return res.json({
         success: true,
         message: "History Collected",
@@ -15,6 +21,14 @@ exports.getOneHistory = async (req, res) => {
         const userId = req.user.id
         const history = await rsvModel.findOneByUserBooked(id, userId)
         console.log(history)
+
+        if(!history){
+            return res.json({
+                success: false,
+                message: `No history with id ${userId} found`
+            })
+        }
+
         return res.json({
             success: true,
             message: `History with id ${id} displayed`,
